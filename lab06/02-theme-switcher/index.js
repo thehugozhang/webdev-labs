2f6134e52fceddcac1b124cf07aa39f2f5859594
Hugo Zhang <hugozhang2023@u.northwestern.edu>
2022-05-06 19:34:37 -0500

    lab06 done

--- lab06/02-theme-switcher/index.js
+++ lab06/02-theme-switcher/index.js
@@ -1,7 +1,17 @@
-/*
-    Hints: 
-    1. Attach click event handlers to all four of the 
-       buttons (#default, #ocean, #desert, and #high-contrast).
-    2. Modify the className property of the body tag 
-       based on the button that was clicked.
-*/
+var body = document.body;
+
+document.getElementById('default').addEventListener('click', function() {
+   body.className = ''
+});
+
+document.getElementById('desert').addEventListener('click', function() {
+   body.className = 'desert'
+});
+
+document.getElementById('ocean').addEventListener('click', function() {
+   body.className = 'ocean'
+});
+
+document.getElementById('high-contrast').addEventListener('click', function() {
+   body.className = 'high-contrast'
+});
--- lab06/03-dyslexia-mode/index.js
+++ lab06/03-dyslexia-mode/index.js
@@ -1,4 +1,25 @@
-/* 
-  See Smashing Magazine Tutorial:
-  https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
-*/
+// Code inspired by https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
+
+const isPressed = window.localStorage.getItem('dyslexic') === 'true';
+
+if(isPressed) {
+  document.querySelector('.content').classList.add('dyslexia-mode');
+  document.getElementById('dyslexia-toggle').classList.add('dyslexia-mode-nav');
+  document.getElementById('header').classList.add('dyslexia-mode-nav-2');
+}
+
+var toggle = document.getElementById('dyslexia-toggle');
+if(isPressed) {
+  toggle.setAttribute('aria-pressed', 'true');
+}
+
+toggle.addEventListener('click', (event) => {
+  let pressed = event.target.getAttribute('aria-pressed') === 'true';
+  event.target.setAttribute('aria-pressed', String(!pressed));
+
+  document.querySelector('.content').classList.toggle('dyslexia-mode');
+  document.getElementById('dyslexia-toggle').classList.toggle('dyslexia-mode-nav');
+  document.getElementById('header').classList.toggle('dyslexia-mode-nav-2');
+
+  window.localStorage.setItem('dyslexic', String(!pressed));
+});
